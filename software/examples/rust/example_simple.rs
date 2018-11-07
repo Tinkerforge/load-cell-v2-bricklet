d42ffd1287097cb66b47cd6b795835d66ba83acf
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:24:20 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

---
+++ software/examples/rust/example_simple.rs
@@ -0,0 +1,25 @@
+use std::{error::Error, io};
+
+use tinkerforge::{ip_connection::IpConnection, load_cell_v2_bricklet::*};
+
+const HOST: &str = "localhost";
+const PORT: u16 = 4223;
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Load Cell Bricklet 2.0.
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let lc = LoadCellV2Bricklet::new(UID, &ipcon); // Create device object.
+
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
+
+    // Get current weight.
+    let weight = lc.get_weight().recv()?;
+    println!("Weight: {} g", weight);
+
+    println!("Press enter to exit.");
+    let mut _input = String::new();
+    io::stdin().read_line(&mut _input)?;
+    ipcon.disconnect();
+    Ok(())
+}
---
+++ software/examples/rust/example_threshold.rs
@@ -0,0 +1,35 @@
+use std::{error::Error, io, thread};
+use tinkerforge::{ip_connection::IpConnection, load_cell_v2_bricklet::*};
+
+const HOST: &str = "localhost";
+const PORT: u16 = 4223;
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Load Cell Bricklet 2.0.
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let lc = LoadCellV2Bricklet::new(UID, &ipcon); // Create device object.
+
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
+
+    // Create receiver for weight events.
+    let weight_receiver = lc.get_weight_receiver();
+
+    // Spawn thread to handle received events. This thread ends when the `lc` object
+    // is dropped, so there is no need for manual cleanup.
+    thread::spawn(move || {
+        for weight in weight_receiver {
+            println!("Weight: {} g", weight);
+        }
+    });
+
+    // Configure threshold for weight "greater than 200 g"
+    // with a debounce period of 1s (1000ms).
+    lc.set_weight_callback_configuration(1000, false, '>', 200, 0);
+
+    println!("Press enter to exit.");
+    let mut _input = String::new();
+    io::stdin().read_line(&mut _input)?;
+    ipcon.disconnect();
+    Ok(())
+}
