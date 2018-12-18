0d68aeabf48c1b1ea81672fea501c2c0fd1f2d4f
Erik Fleckstein <erik@tinkerforge.com>
2018-12-18 14:57:20 +0100

    go: Change import path.

--- software/examples/go/example_threshold.go
+++ software/examples/go/example_threshold.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"tinkerforge/ipconnection"
-	"tinkerforge/load_cell_v2_bricklet"
+	"github.com/tinkerforge/go-api-bindings/ipconnection"
+	"github.com/tinkerforge/go-api-bindings/load_cell_v2_bricklet"
 )
 
 const ADDR string = "localhost:4223"
