6c8cff22895a040287def11e04dfcda1f974ba33
Erik Fleckstein <erik@tinkerforge.com>
2018-12-20 16:56:33 +0100

    rust: Reformat examples.

--- software/examples/rust/example_simple.rs
+++ software/examples/rust/example_simple.rs
@@ -1,8 +1,6 @@
-use std::{io, error::Error};
+use std::{error::Error, io};
 
-use tinkerforge::{ip_connection::IpConnection, 
-                  load_cell_v2_bricklet::*};
-
+use tinkerforge::{ip_connection::IpConnection, load_cell_v2_bricklet::*};
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -13,11 +11,11 @@ fn main() -> Result<(), Box<dyn Error>> {
     let lc = LoadCellV2Bricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-    // Don't use device before ipcon is connected.
+                                          // Don't use device before ipcon is connected.
 
-		// Get current weight.
-let weight = lc.get_weight().recv()?;
-		println!("Weight: {} g", weight);
+    // Get current weight.
+    let weight = lc.get_weight().recv()?;
+    println!("Weight: {} g", weight);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
